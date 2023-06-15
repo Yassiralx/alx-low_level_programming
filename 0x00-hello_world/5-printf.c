8edfb94a53ae3baaedfdcf1f255c373c174292a8
Yassiralx <yyass7589@gmail.com>
2023-06-15 16:03:41 -0700

    jfegfhjefhefjf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is a C program that prints using printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+
+
+}
